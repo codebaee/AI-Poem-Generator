a6da99c7be728cfa46e7088c64a0870f62a61bc1
Ashley <codingbaee@gmail.com>
2024-02-17 17:07:38 -0500

    Improved UX

--- src/script.js
+++ src/script.js
@@ -20,6 +20,12 @@ function generatePoem(event) {
     "User instructions: You are a Poem expert and love to write short poems. Your mission is to to generate a 4 line poem in basic HTML. Be sure to follow the user instructions. Also always have a title that is in a <strong> element. Please make sure that there are line breaks to separte sentences on their own line. <br> ";
   let apiKey = "d010da332cob3740f398ft7aa7bdef74";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemOutput = document.querySelector("#poem");
+  poemOutput.classList.remove("hidden");
+  poemOutput.innerHTML = `<div class= "blink">⏳ Generating your poem about ${instructionsInput.value} </div> `;
+
+  //
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
